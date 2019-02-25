931fbf391a3fac4cb27356a54d54b6fc65a23bbf
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-25 13:59:05 +0100

    head

---
+++ head.js
@@ -0,0 +1,19 @@
+const arrayA = [1, 2, 3, 4, 5];
+const arrayB = [];
+const string = "this is a string";
+
+function head(arr) {
+  let returnVal;
+  if (arr.length === 0) {
+    returnVal = undefined
+  } else if (!Array.isArray(arr)){
+    returnVal = 'This is not an array!'
+  } else {
+    returnVal = arr[0]
+  }
+  return returnVal
+}
+
+console.log(head(arrayA));
+console.log(head(arrayB));
+console.log(head(string));
