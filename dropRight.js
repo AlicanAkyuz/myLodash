eedfa2444accf7e0c7ff92883ddf24beaf3a9f1e
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-25 14:31:45 +0100

    dropright.

---
+++ dropRight.js
@@ -0,0 +1,13 @@
+const arrayA = [1, 2, 3, 4, 5];
+const arrayB = ['sun', 'rain', 'snow', 'fog', 'mist'];
+
+function drop(arr, n) {
+  const newArr = [];
+  for (let i = 0; i < arr.length - n; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr
+}
+
+console.log(drop(arrayA, 3));
+console.log(drop(arrayB, 2));
