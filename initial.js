ad8d2bfb5e7dac64851c68014b24e2ffe50d0e44
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-25 13:50:03 +0100

    initial.

---
+++ initial.js
@@ -0,0 +1,17 @@
+const arrayA = ['a', 'b', 'c'];
+const arrayB = ['10', '9', '8'];
+
+function initial(arr) {
+  let newArr = [];
+  for (const item of arr) {
+    if (arr.indexOf(item) === arr.length - 1) {
+      continue
+    } else {
+      newArr.push(item)
+    }
+  }
+  return newArr
+}
+
+console.log(initial(arrayA));
+console.log(initial(arrayB));
