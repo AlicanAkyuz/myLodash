bb9277da68c5c46f0f87cbeb35919a2453f76f34
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-25 13:52:43 +0100

    join.

---
+++ join.js
@@ -0,0 +1,15 @@
+const arrayA = [1, 2, 3, 4];
+const arrayB = [5, 'a', 'b', 6];
+const seperatorA = '*';
+const seperatorB = '-';
+
+function join(arr, seperator) {
+  let string = '';
+  for (const item of arr) {
+    arr.indexOf(item) === 0 ? string = item : string = string + seperator + item;
+  }
+  return string
+}
+
+console.log(join(arrayB, seperatorB));
+console.log(join(arrayA, seperatorA));
