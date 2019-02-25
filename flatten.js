8b2495f7efcaaa6e95b5bb49162963cb34adcec2
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-25 13:50:54 +0100

    flatten.

---
+++ flatten.js
@@ -0,0 +1,17 @@
+const myArray = [0, 1, 2, [3, 4], [5, [6, 7]]];
+
+function flatten(arr) {
+  let newArr = [];
+  for (const item of arr) {
+    if (!Array.isArray(item)) {
+      newArr.push(item)
+    } else {
+      for (const element of item) {
+        newArr.push(element)
+      }
+    }
+  }
+  return newArr
+}
+
+console.log(flatten(myArray));
