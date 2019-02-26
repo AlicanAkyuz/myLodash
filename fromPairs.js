a8937de66ca3bf65c92a6d1b25bef4c0b72f76d4
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-02-26 10:58:41 +0100

    fromPairs.

--- fromPairs.js
+++ fromPairs.js
@@ -1,1 +1,20 @@
+const arrayA = [['a', 1], ['b', 2], ['c', 3]];
+const arrayB = [['a'], [2], ['c', 3]];
 
+function fromPairs(arr) {
+  let returnVal;
+  let obj = {};
+
+  for (const item of arr) {
+    if (item.length !== 2) {
+      returnVal = "Please provide key-value pairs as array elements"
+      return returnVal
+    } else {
+      obj[item[0]] = item[1]
+    }
+  }
+  return obj
+}
+
+console.log(fromPairs(arrayA));
+console.log(fromPairs(arrayB));
