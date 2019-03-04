728c8f4b074b2953cd0b6af4c1bc37fd4e4ff42e
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-04 14:54:14 -0800

    chunk.

---
+++ Array/chunk.js
@@ -0,0 +1,14 @@
+// Creates an array of elements split into groups the length of size.
+// If array can't be split evenly, the final chunk will be the remaining elements.
+
+const arrayA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function Chunk(arr, size) {
+  const newArr = [];
+  for (let i = 1; arr.length / size; i++) {
+    newArr.push(arr.splice(0, size))
+  }
+  return newArr
+};
+
+console.log(Chunk(arrayA, 3));
