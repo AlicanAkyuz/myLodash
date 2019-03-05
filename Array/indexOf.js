570f3ce03dd55bca42b7fde607459b2ca22ef8cb
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-05 10:20:40 -0800

    indexOf.

--- Array/indexOf.js
+++ Array/indexOf.js
@@ -2,12 +2,22 @@
 
 function indexOf(arr, value, fromIndex) {
   let index;
-  for (let i = fromIndex; i < arr.length; i++) {
-    if (arr[i] === value) {
-      index = i;
-      break
+  if (fromIndex) {
+    for (let i = fromIndex; i < arr.length; i++) {
+      if (arr[i] === value) {
+        index = i;
+        break
+      }
+    }
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        index = i;
+        break
+      }
     }
   }
+
   if (index === undefined) {
     index = -1
   }
@@ -18,3 +28,4 @@ const arrayA = [1, 2, 3, 4, 5, 6, 7, 12, 20];
 
 console.log(indexOf(arrayA, 20, 7));
 console.log(indexOf(arrayA, 3, 1));
+console.log(indexOf(arrayA, 5));
