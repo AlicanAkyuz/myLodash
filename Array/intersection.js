f33a115a143933ca66795937be4b647ef2fb334b
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-05 21:06:58 -0800

    intersection.

--- Array/intersection.js
+++ Array/intersection.js
@@ -2,24 +2,21 @@
 // The order and references of result values are determined by the first array.
 
 function intersection() {
-  let newArr = [...arguments];
-  let firstArr = [...newArr[0]];
-  let storeArr = [];
-  for (let i = 1; i < newArr.length; i++) {
-    firstArr.forEach(e => {
-      for (let y = 0; y < newArr[i].length; y++) {
-        if (e === newArr[i][y]) {
-          storeArr.push(e);
-        }
-      }
-    })
-  };
-  return [...new Set(storeArr)]
+  let argArr = [...arguments];
+  let newArr = argArr.shift().filter(e => argArr[0].includes(e));
+
+  if (argArr.length > 1) {
+    argArr.shift();
+    argArr.unshift(newArr);
+    newArr = intersection(...argArr);
+  }
+
+  return newArr
 }
 
-const arrayA = [1, 13, 3, 15, 'e'];
-const arrayB = [5, 'e', 15, 4, 13];
-const arrayC = [6, 15, 7, 13, 'e', 72, 16];
-const arrayD = ['e', 15, 8, 13, 9];
+const arrayA = [1, 13, 3, 'e', 28, 'h'];
+const arrayB = ['h', 5, 'e', 28, 15, 4, 13];
+const arrayC = [6, 'h', 15, 7, 13, 'e', 72, 16];
+const arrayD = ['e', 15, 8, 13, 9, 'h'];
 
 console.log(intersection(arrayA, arrayB, arrayC, arrayD));
