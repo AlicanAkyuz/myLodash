f145c5ebc82d0e08a07fc394eb7e72f4abd8cadd
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-15 12:55:18 -0700

    padRight.

--- String/padRight.js
+++ String/padRight.js
@@ -2,7 +2,6 @@
 // Padding characters are truncated if they exceed length.
 // If no padding character provided, the string is padded with spaces.
 
-
 function padRight(str, length, char) {
   let newStr = '';
   let charToAdd = '';
@@ -10,8 +9,18 @@ function padRight(str, length, char) {
 
   if (str.length === length) {
     return str
-  } else {
-    return padRight(newStr = str.concat(charToAdd[0]), length, charToAdd)
+  }
+
+  if (str.length > length) {
+    for (let i = 0; i < str.length -1; i++) {
+      newStr = newStr.concat(str[i]);
+    }
+    return padRight(newStr, length, charToAdd)
+  }
+
+  if (str.length < length) {
+    newStr = str.concat(charToAdd)
+    return padRight(newStr, length, charToAdd)
   }
 };
 
@@ -19,4 +28,4 @@ const strA = 'abc';
 console.log(padRight(strA, 3));
 console.log(padRight(strA, 5));
 console.log(padRight(strA, 7, '**'));
-console.log(padRight(strA, 10, '-_'));
+console.log(padRight(strA, 10, '-_)'));
